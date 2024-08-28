aea45d4a6ca970ec4c810511450610cb26e3f680
VixhoMery <vicentemeryarancibia@gmail.com>
2024-08-28 11:07:45 -0400

    Implementación setters y getters clase Persona

--- Persona.java
+++ Persona.java
@@ -1,3 +1,41 @@
 public class Persona {
     private String nombre;
+    private String rut;
+    private String correo;
+    private String direccion;
+
+    //Setters
+    public void setNombre(String name){
+        nombre = name;
+    }
+
+    public void setRut(String rut){
+        this.rut = rut;
+    }
+
+    public void setCorreo(String mail){
+        correo = mail;
+    }
+
+    public void setDireccion(String adress){
+        direccion = adress;
+    }
+
+    //Getters
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getRut(){
+        return rut;
+    }
+
+    public String getCorreo(){
+        return correo;
+    }
+
+    public String getDireccion(){
+        return direccion;
+    }
 }
