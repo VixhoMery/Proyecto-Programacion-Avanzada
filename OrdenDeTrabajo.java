9ced8b2b23c08a02604ea130308c53223f833393
Joaquin Cornejo <joaquincornejof.12@gmail.com>
2024-08-28 22:41:48 -0400

    Creación de clase OrdenDeTrabajo, setter y getter

---
+++ OrdenDeTrabajo.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+public class OrdenDeTrabajo {
+    private Persona cliente;
+    private String problema;
+    private String diagnostico;
+    private ArrayList<OrdenDeTrabajo> piezasNecesarias = new ArrayList<>();
+    private String fechaRecepcion;
+    private String fechaEntregaEstimada;
+    //Setter
+    public void setCliente(Persona cc){
+        cliente = cc;
+    }
+    public void setProblema(String problem){
+        problema = problem;
+    }
+    public void setDiagnostico(String diag){
+        diagnostico = diag;
+    }
+    public void setPiezasNecesarias(ArrayList<OrdenDeTrabajo> piezas){
+        piezasNecesarias = piezas;
+    }
+    public void setFechaRecepcion(String fechaR){
+        fechaRecepcion = fechaR;
+    }
+    public void setFechaEntregaEstimada(String fechaR){
+        fechaEntregaEstimada = fechaR;
+    }
+
+    //Getter
+    public Persona getCliente(){
+        return cliente;
+    }
+    public String getProblema(){
+        return problema;
+    }
+    public String getDiagnostico(){
+        return diagnostico;
+    }
+    public ArrayList getPiezasNecesarias(){
+        return piezasNecesarias;
+    }
+    public String getFechaRecepcion(){
+        return fechaRecepcion;
+    }
+    public String getFechaEntregaEstimada(){
+        return fechaEntregaEstimada;
+    }
+}
