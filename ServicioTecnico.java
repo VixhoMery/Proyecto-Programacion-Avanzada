51ee37cb0041599614427975802818853d0fe3da
Txmas <tomyolivares12@gmail.com>
2024-08-28 23:30:07 -0400

    Creacion de clase Estante y clase ServicioTecnico

---
+++ ServicioTecnico.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class ServicioTecnico {
+    HashMap<String, Estante> estantes = new HashMap<>();
+
+    void agregarEstante(String tipoTrabajo) {
+        estantes.put(tipoTrabajo, new Estante());
+    }
+
+    void agregarOrden(String tipoTrabajo, OrdenDeTrabajo orden) {
+        estantes.get(tipoTrabajo).agregarOrden(orden);
+    }
+}
