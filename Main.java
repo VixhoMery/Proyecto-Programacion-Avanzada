357d03fec5ed0cd130c10027f4c3037cb02f991b
VixhoMery <vicentemeryarancibia@gmail.com>
2024-08-29 17:11:20 -0400

    Actualización del main

--- Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String arg[]){
-        //Persona cliente1 = new Persona("Carlos Meneses", "21.768.545-1", "carlos@mail.cl", "ola", 123456);
-        
-    }
-}
---
+++ Menu.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+public class Menu {
+
+    static Scanner leerDato= new Scanner (System.in);
+
+    public static void main(String arg[]){
+
+        System.out.println("              ======================");
+        System.out.println("              ===Servicio Tecnico===");
+        System.out.println("              ======================");
+        System.out.println("                     █████████");
+        System.out.println("  ███████          ███▒▒▒▒▒▒▒▒███");
+        System.out.println("  █▒▒▒▒▒▒█       ███▒▒▒▒▒▒▒▒▒▒▒▒▒███");
+        System.out.println("   █▒▒▒▒▒▒█    ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██");
+        System.out.println("    █▒▒▒▒▒█   ██▒▒▒▒▒██▒▒▒▒▒▒██▒▒▒▒▒███");
+        System.out.println("     █▒▒▒█   █▒▒▒▒▒▒████▒▒▒▒████▒▒▒▒▒▒██");
+        System.out.println("   █████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██");
+        System.out.println("   █▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒██");
+        System.out.println(" ██▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒██▒▒▒▒▒▒▒▒▒▒██▒▒▒▒██");
+        System.out.println("██▒▒▒███████████▒▒▒▒▒██▒▒▒▒▒▒▒▒██▒▒▒▒▒██");
+        System.out.println("█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒████████▒▒▒▒▒▒▒██");
+        System.out.println("██▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██");
+        System.out.println(" █▒▒▒███████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██");
+        System.out.println(" ██▒▒▒▒▒▒▒▒▒▒████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█");
+        System.out.println("  ████████████   █████████████████\n");
+
+        //Limpiar pantalla.
+        try {
+            Thread.sleep(4000);  // Pausa de 4 segundos
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // Imprimir 50 líneas en blanco
+        for (int i = 0; i < 50; i++) {
+            System.out.println();
+        }
+        
+       
+
+        int opcion ; 
+
+        do{
+            //Menu de opciones
+            System.out.println("======================");
+            System.out.println("==Ingrese una Opción==");
+            System.out.println("======================");
+            System.out.println("1) Registrar Cliente");
+            System.out.println("2) OPCION 2");
+            System.out.println("3) OPCION 3");
+            System.out.println("4) OPCION 4\n");
+            System.out.println("5) Salir del programa\n");
+
+            opcion = leerDato.nextInt(); 
+
+            //Switch de casos posibles
+            switch (opcion){
+
+                case 1:
+                    System.out.println("Seleccionaste la opcion 1");
+                    break;
+
+                case 2:
+                    System.out.println("Seleccionaste la opcion 2");
+                    break;
+
+                case 3:
+                    System.out.println("Seleccionaste la opcion 3");
+                    break;
+                
+                case 4:
+                    System.out.println("Seleccionaste la opcion 4");
+                    break;
+                
+                case 5:
+                    break;    
+                }
+            }while(opcion != 5);
+
+            System.out.println("Hasta Luego!!!");
+        }
+    }
