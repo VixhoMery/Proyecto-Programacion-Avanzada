15cc1ac7f94c3a2c2d181f4617a4b5b3cee05a62
VixhoMery <vicentemeryarancibia@gmail.com>
2024-08-28 22:41:00 -0400

    Actualización y reinserción de Persona.java

--- Persona.java
+++ Persona.java
@@ -3,39 +3,38 @@ public class Persona {
     private String rut;
     private String correo;
     private String direccion;
+    private int telefono;
 
     //Setters
     public void setNombre(String name){
         nombre = name;
     }
-
     public void setRut(String rut){
         this.rut = rut;
     }
-
     public void setCorreo(String mail){
         correo = mail;
     }
-
     public void setDireccion(String adress){
         direccion = adress;
     }
-
+    public void setTelefono(int fono){
+        telefono = fono;
+    }
     //Getters
-
     public String getNombre(){
         return nombre;
     }
-
     public String getRut(){
         return rut;
     }
-
     public String getCorreo(){
         return correo;
     }
-
     public String getDireccion(){
         return direccion;
+    }
+    public int getTelefono(){
+        return telefono;
     }
 }
