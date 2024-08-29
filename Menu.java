dfe4e13551b2ca6942da4ad9da9342bae30214b7
Txmas <tomyolivares12@gmail.com>
2024-08-29 16:47:48 -0400

    Creacion y avance de menu 1.0

--- Menu.java
+++ Menu.java
@@ -1,8 +1,10 @@
-//import java.io.*;
+import java.util.Scanner;
 public class Menu {
+
+    static Scanner leerDato= new Scanner (System.in);
+
     public static void main(String arg[]){
-        //BufferedReader leerDato = new BufferedReader (new InputStreamReader(System.in));
-        
+
         System.out.println("              ======================");
         System.out.println("              ===Servicio Tecnico===");
         System.out.println("              ======================");
@@ -32,14 +34,48 @@ public class Menu {
         for (int i = 0; i < 50; i++) {
             System.out.println();
         }
+        
+       
 
-        System.out.println("======================");
-        System.out.println("==Ingrese una Opción==");
-        System.out.println("======================");
-        System.out.println("1) OPCION 1");
-        System.out.println("2) OPCION 2");
-        System.out.println("3) OPCION 3");
-        System.out.println("4) OPCION 4\n");
+        int opcion ; 
 
+        do{
+            //Menu de opciones
+            System.out.println("======================");
+            System.out.println("==Ingrese una Opción==");
+            System.out.println("======================");
+            System.out.println("1) OPCION 1");
+            System.out.println("2) OPCION 2");
+            System.out.println("3) OPCION 3");
+            System.out.println("4) OPCION 4\n");
+            System.out.println("5) Salir del programa\n");
+
+            opcion = leerDato.nextInt(); 
+
+            //Switch de casos posibles
+            switch (opcion){
+
+                case 1:
+                    System.out.println("Seleccionaste la opcion 1");
+                    break;
+
+                case 2:
+                    System.out.println("Seleccionaste la opcion 2");
+                    break;
+
+                case 3:
+                    System.out.println("Seleccionaste la opcion 3");
+                    break;
+                
+                case 4:
+                    System.out.println("Seleccionaste la opcion 4");
+                    break;
+                
+                case 5:
+                    break;    
+                }
+            }while(opcion != 5);
+
+            System.out.println("Hasta Luego!!!");
+        }
     }
-}
